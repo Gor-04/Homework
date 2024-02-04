c479c3656ba0a5e292727e495851924b99bff48f
Gor04 <gor.grigoryan0609@gmail.com>
2024-02-04 21:16:01 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+ 
+        static void Main(string[] args)
+        {
+            
+            
+            /*  Console.WriteLine("Enter the temperature");
+              int temp = Convert.ToInt32(Console.ReadLine());
+              if (temp < 0)  Console.WriteLine("Freezing weather.");    
+              else if (temp >= 0 && temp < 10)  Console.WriteLine("Cold weather."); 
+              else if (temp >= 10 && temp < 30)Console.WriteLine("Cool weather."); 
+              else Console.WriteLine("Hot weather");*/
+
+            /*Console.WriteLine("Enter the age");
+            int age = Convert.ToInt32(Console.ReadLine());
+            if (age < 0) Console.WriteLine("Invalid input");
+            else if (age >= 0 && age < 13) Console.WriteLine("Child");
+            else if (age >= 13 && age <= 19) Console.WriteLine("teenager");
+            else Console.WriteLine("Adult");*/
+
+            /*    Console.WriteLine("Select the option");
+                Console.WriteLine("Press 1 for F to C");
+                Console.WriteLine("Press 2 for C to F");
+                int num = Convert.ToInt32(Console.ReadLine());
+                if(num == 1)
+                {
+                    Console.WriteLine("Input temp in F");
+                    double temp = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("C is " + (temp - 32) * 5 / 9);
+                }
+                else if (num == 2)
+                {
+                    Console.WriteLine("Input temp in C");
+                    double temp1 = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("F is " + ((temp1) * 9 / 5 + 32));
+                }
+                else Console.WriteLine("Invalid option. Please select 1 or 2.");*/
+
+            /*  Console.WriteLine("Input number of elements");
+              int n = Convert.ToInt32(Console.ReadLine());
+              int[] arr = new int[n];
+              Console.WriteLine("Input elements");
+              for (int i = 0; i < arr.Length; i++)
+              {
+                  arr[i] = Convert.ToInt32(Console.ReadLine());
+              }
+              int mn = arr[0];
+              int mx = arr[0];
+              int scdndlarg = arr[0];
+
+              for(int i = 0; i < arr.Length; i++)
+              {
+                  if (mn > arr[i]) mn = arr[i];
+                  if(mx < arr[i]) mx = arr[i];
+              }
+              for (int i = 0; i < arr.Length; i++)
+              {
+                  if (mx != arr[i])
+                  {
+                      if (scdndlarg < arr[i])scdndlarg = arr[i];
+                  }
+              }
+              Console.WriteLine("Minimum is " + mn);
+              Console.WriteLine("Maximum is " + mx);
+              Console.WriteLine("Second Largest is " + scdndlarg);
+              for (int i = 0; i < n / 2; i++)
+              {
+                  int tmp = arr[i];
+                  arr[i] = arr[n - i - 1];
+                  arr[n - i - 1] = tmp;
+              }
+              Console.WriteLine("After Reversing array is");
+              for (int i = 0; i < n; i++)
+              {
+                  Console.WriteLine(arr[i]);
+              }
+  */
+
+            // Last One I have done in the classroom :D 
+            
+    
+
+        }
+    }
+}
